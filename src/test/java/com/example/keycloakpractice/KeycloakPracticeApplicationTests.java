b061aa574f6218b133bd6e82c77349e943930696
Lester Deeley <lester.deeley@yahoo.com>
2023-01-24 12:33:16 +0000

    Initial

---
+++ src/test/java/com/example/keycloakpractice/KeycloakPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloakpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
